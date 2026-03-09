abd498bfd865c09df13773fbbbd2e8d4b56d6bf2
Kauan Silva <kauanmacedo338@gmail.com>
2026-03-09 16:25:05 -0300

    remoção de arquivos inuteis

--- JogoDeAdivinhacao.ConsoleApp1/obj/Debug/net10.0/JogoDeAdivinhacao.ConsoleApp1.AssemblyInfo.cs
+++ JogoDeAdivinhacao.ConsoleApp1/obj/Debug/net10.0/JogoDeAdivinhacao.ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JogoDeAdivinhacao.ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a7f7139cdcb05af7d01acbf1d76f359901013d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d29494a1f700892f364aa9df36ac21db0817a1ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("JogoDeAdivinhacao.ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JogoDeAdivinhacao.ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
