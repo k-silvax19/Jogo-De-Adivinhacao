1a7f7139cdcb05af7d01acbf1d76f359901013d5
Kauan Silva <kauanmacedo338@gmail.com>
2026-03-09 15:42:11 -0300

    first commit

---
+++ JogoDeAdivinhacao.ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("-----------------------------------");
+Console.WriteLine("Jogo De Adivinhacao");
+Console.WriteLine("-----------------------------------");
+
+Console.Write("Digite Um Numero De 1 - 10: ");
+string? chute = Console.ReadLine();
+
+
+Console.ReadLine();
+
+
